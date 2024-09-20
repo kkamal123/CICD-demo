b5d7aed8951d18c494745be319e233e17dfaf155
kkamal123 <85450398+kkamal123@users.noreply.github.com>
2024-09-20 17:07:53 +0500

    Create app.js

---
+++ app/microservice-2/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Microservice 2 says Hello!'));
+
+app.listen(PORT, () => console.log(`Microservice 2 running on port ${PORT}`));
