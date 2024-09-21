3191f84fcfa90494ac9224cff3605aa06e20915d
kkamal123 <85450398+kkamal123@users.noreply.github.com>
2024-09-21 15:44:03 +0500

    Update app.js

--- app/microservice-2/app.js
+++ app/microservice-2/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Microservice 2 says Hello!'));
+app.get('/', (req, res) => res.send('Microservice 2 says Hello! change-1'));
 
 app.listen(PORT, () => console.log(`Microservice 2 running on port ${PORT}`));
